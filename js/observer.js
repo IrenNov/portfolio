42691bda64d2a7d1ec245217623990e2aa35ad31
IrenNov <irennov524@gmail.com>
2024-11-01 19:58:11 +0300

    section aboutme

---
+++ js/observer.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            const textElements = entry.target.querySelectorAll('.animate');
+
+            if (entry.isIntersecting) {
+                textElements.forEach(textElement => {
+                    // Удаляем out-of-view и добавляем in-view при попадании в зону видимости
+                    textElement.classList.remove('out-of-view');
+                    textElement.classList.add('in-view');
+                });
+            } else {
+                textElements.forEach(textElement => {
+                    // Удаляем in-view и добавляем out-of-view при выходе из зоны видимости
+                    textElement.classList.remove('in-view');
+                    textElement.classList.add('out-of-view');
+                });
+            }
+        });
+    }, {
+        root: null, // viewport
+        threshold: 0.8 // запуск анимации при 10% видимости
+    });
+
+    // Наблюдаем за секцией с id 'aboutme'
+    const targetSection = document.getElementById('aboutme');
+    if (targetSection) {
+        observer.observe(targetSection);
+    }
+});
